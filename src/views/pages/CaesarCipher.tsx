b889a619c4d015e09fa309d3b965beca16f6cc50
SolomDev00 <ew11solom@gmail.com>
2024-12-19 20:55:48 +0200

    CaesarCipher is done.

--- src/views/pages/CaesarCipher.tsx
+++ src/views/pages/CaesarCipher.tsx
@@ -1,79 +1,121 @@
 import { useState } from "react";
 import Input from "../../components/ui/Input";
 import Button from "../../components/ui/Button";
+import { SoLock, SoSecurityLock, SoSquareLockCheck } from "solom-icon";
+import toast from "react-hot-toast";
 
 const CipherPage = () => {
-  const [key, setKey] = useState<number>(3);
-  const [plaintext, setPlaintext] = useState<string>("");
-  const [ciphertext, setCiphertext] = useState<string>("");
+    const [key, setKey] = useState<number>(3);
+    const [plaintext, setPlaintext] = useState<string>("");
+    const [ciphertext, setCiphertext] = useState<string>("");
 
-  const encrypt = () => {
-    const encryptedText = plaintext
-      .split("")
-      .map((char) => {
-        if (char.match(/[a-z]/i)) {
-          const base = char.charCodeAt(0) < 97 ? 65 : 97;
-          return String.fromCharCode(
-            ((char.charCodeAt(0) - base + key) % 26) + base
-          );
+    const encrypt = () => {
+        if (!plaintext) {
+            toast.error("الرجاء إدخال نص للتشفير");
+            return;
         }
-        return char;
-      })
-      .join("");
-    setCiphertext(encryptedText);
-  };
 
-  const decrypt = () => {
-    const decryptedText = ciphertext
-      .split("")
-      .map((char) => {
-        if (char.match(/[a-z]/i)) {
-          const base = char.charCodeAt(0) < 97 ? 65 : 97;
-          return String.fromCharCode(
-            ((char.charCodeAt(0) - base - key + 26) % 26) + base
-          );
+        const encryptedText = plaintext
+            .split("")
+            .map((char) => {
+                if (char.match(/[a-zA-Z]/)) {
+                    const base = char.charCodeAt(0) < 97 ? 65 : 97;
+                    return String.fromCharCode(
+                        ((char.charCodeAt(0) - base + key) % 26) + base
+                    );
+                } else if (char.match(/[ء-ي]/)) {
+                    const base = 0x0621;
+                    const range = 28;
+                    const charCode = char.charCodeAt(0);
+                    const newCharCode = ((charCode - base + key) % range + range) % range + base;
+                    return String.fromCharCode(newCharCode);
+                }
+                return char;
+            })
+            .join("");
+        setCiphertext(encryptedText);
+    };
+
+    const decrypt = () => {
+        if (!ciphertext) {
+            toast.error("الرجاء إدخال نص مشفر لفك التشفير");
+            return;
         }
-        return char;
-      })
-      .join("");
-    setPlaintext(decryptedText);
-  };
 
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Encryption & Decryption</h1>
+        const decryptedText = ciphertext
+            .split("")
+            .map((char) => {
+                if (char.match(/[a-zA-Z]/)) {
+                    const base = char.charCodeAt(0) < 97 ? 65 : 97;
+                    return String.fromCharCode(
+                        ((char.charCodeAt(0) - base - key + 26) % 26) + base
+                    );
+                } else if (char.match(/[ء-ي]/)) {
+                    const base = 0x0621;
+                    const range = 28;
+                    const charCode = char.charCodeAt(0);
+                    const newCharCode = ((charCode - base - key + range) % range + range) % range + base;
+                    return String.fromCharCode(newCharCode);
+                }
+                return char;
+            })
+            .join("");
+        setPlaintext(decryptedText);
+    };
 
-      <div className="mb-4">
-        <label className="block mb-2">Key (k):</label>
-        <Input
-          type="number"
-          value={key}
-          onChange={(e) => setKey(Number(e.target.value))}
-        />
-      </div>
 
-      <div className="mb-4">
-        <label className="block mb-2">Plaintext:</label>
-        <Input
-          type="text"
-          value={plaintext}
-          onChange={(e) => setPlaintext(e.target.value)}
-        />
-      </div>
-      <div className="flex items-center gap-3">
-        <Button fullWidth onClick={encrypt}>
-          Encrypt
-        </Button>
-        <Button fullWidth onClick={decrypt} variant={"danger"}>
-          Decrypt
-        </Button>
-      </div>
-      <div className="mt-4">
-        <h2 className="text-lg font-semibold">Ciphertext:</h2>
-        <p className="p-2 border">{ciphertext}</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="p-4">
+            <h1 className="text-2xl font-bold text-primary mb-4">
+                Encryption & Decryption
+            </h1>
+            <div className="w-full flex flex-row items-center gap-2 border border-accent rounded-lg relative mb-3">
+                <div className="bg-accent text-white p-2 pr-20 rounded-l-md flex items-center gap-3">
+                    <SoLock className="w-5 h-5" />
+                    <span className="text-base font-semibold w-fit max-sm:hidden">Key</span>
+                </div>
+                <Input
+                    type="number"
+                    value={key}
+                    onChange={(e) => setKey(Number(e.target.value))}
+                />
+            </div>
+            <div className="w-full flex flex-row items-center gap-2 border border-accent rounded-lg relative mb-3">
+                <div className="bg-accent text-white p-2 pr-10 rounded-l-md flex items-center gap-3">
+                    <SoSecurityLock className="w-5 h-5" />
+                    <span className="text-base font-semibold w-fit max-sm:hidden">
+                        Plaintext
+                    </span>
+                </div>
+                <Input
+                    type="text"
+                    value={plaintext}
+                    onChange={(e) => setPlaintext(e.target.value)}
+                />
+            </div>
+            <div className="w-full flex flex-row items-center gap-2 border border-accent rounded-lg relative mb-5">
+                <div className="bg-accent text-white p-2 pr-8 rounded-l-md flex items-center gap-3">
+                    <SoSquareLockCheck className="w-5 h-5" />
+                    <span className="text-base font-semibold w-fit max-sm:hidden">
+                        Ciphertext
+                    </span>
+                </div>
+                <Input
+                    type="text"
+                    value={ciphertext}
+                    onChange={(e) => setCiphertext(e.target.value)}
+                />
+            </div>
+            <div className="flex items-center gap-3">
+                <Button fullWidth onClick={encrypt}>
+                    Encrypt
+                </Button>
+                <Button fullWidth onClick={decrypt} variant={"danger"}>
+                    Decrypt
+                </Button>
+            </div>
+        </div>
+    );
 };
 
 export default CipherPage;
